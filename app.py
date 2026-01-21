fd62d7f91974403086a88cf5e6f35a460c4d4aa6
Raphael <raphaelj.dev@gmail.com>
2026-01-21 16:57:41 -0300

    Refatora funcionalidades

--- app.py
+++ app.py
@@ -23,8 +23,7 @@ def exibir_opcoes():
 """)
   
 def finalizar_programa():
-  os.system('clear')
-  print('Programa finalizado.')
+  exibir_subtitulo('Programa finalizado.')
 
 def voltar_ao_menu_principal():
   input('\nDigite uma tecla para voltar ao menu principal\n')
@@ -34,9 +33,13 @@ def opcao_invalida():
   print('Condicao invalida\n')
   voltar_ao_menu_principal()
 
-def cadastrar_novo_restaurante():
+def exibir_subtitulo(texto):
   os.system('clear')
-  print('Cadastro de novos restaurantes\n')
+  print(texto)
+  print()
+
+def cadastrar_novo_restaurante():
+  exibir_subtitulo('Cadastro de novos restaurantes')
   nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
   restaurantes.append(nome_do_restaurante)
   print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso.')
@@ -44,8 +47,7 @@ def cadastrar_novo_restaurante():
 
 
 def listar_restaurantes():
-  os.system('clear')
-  print('Listando os restaurantes:\n')
+  exibir_subtitulo('Listando os restaurantes:')
   for restaurante in restaurantes:
     print(f'.{restaurante}')
   voltar_ao_menu_principal()
